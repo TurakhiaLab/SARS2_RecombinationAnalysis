b10a246f49c0a969b1bc62ec58ddcb5919466937
mrkylesmith <soccerkws@gmail.com>
2025-07-07 12:12:59 -0700

    figure s4 minor formatting improvements

--- figures/supplemental/s4/static/bar.js
+++ figures/supplemental/s4/static/bar.js
@@ -5,7 +5,6 @@ async function bar(svg, config) {
   const height = config["height"];
   const KEY = config["key"];
   const VALUE = config["value"];
-
   const data = await d3.csv(config["data"]);
   const MAX = parseInt(d3.max(data)[VALUE]);
 
@@ -14,12 +13,15 @@ async function bar(svg, config) {
     .scaleLinear()
     .domain([0, MAX + 1])
     .range([0, width]);
-  svg
+  const xAxis = d3.axisBottom(x);
+
+  let xAxisLine = svg
     .append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
+    .call(xAxis)
     .selectAll("text")
     .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("font-size", config["axis-tick-size"])
     .style("text-anchor", "end");
 
   // Add x axis title
@@ -27,12 +29,12 @@ async function bar(svg, config) {
     .append("text")
     .attr("class", "x label")
     .attr("text-anchor", "end")
-    .attr("x", width / 2)
-    .attr("y", height + 50)
+    .attr("x", width / 1.8)
+    .attr("y", height + 60)
     .attr("dx", ".75em")
-    .style("font-size", "14px")
+    .style("font-size", config["axis-title-size"])
     .style("fill", "black")
-    .text("Frequency");
+    .text(config["x-title"]);
 
   // y-axis
   const y = d3
@@ -40,11 +42,11 @@ async function bar(svg, config) {
     .range([0, height])
     .domain(
       data.map(function (d) {
-        return d[KEY];
+        const key = config["scaleBandMap"][d[KEY]];
+        return key;
       }),
     )
     .padding(0.1);
-
 
   // Horizontal Bars
   svg
@@ -54,26 +56,33 @@ async function bar(svg, config) {
     .append("rect")
     .attr("x", x(0))
     .attr("y", function (d) {
-      return y(d[KEY]);
+      const key = config["scaleBandMap"][d[KEY]];
+      return y(key);
     })
     .attr("width", function (d) {
       return x(d[VALUE]);
     })
     .attr("height", y.bandwidth())
-    .attr("fill", config['bar-color']);
+    .attr("fill", config["bar-color"]);
 
+  let yAxis = d3.axisLeft(y);
   // Add y-axis at end, after bars have rendered
-  svg.append("g").call(d3.axisLeft(y));
+  svg
+    .append("g")
+    .call(yAxis)
+    .selectAll("text")
+    .style("font-size", config["axis-tick-size"])
+    .style("text-anchor", "end");
 
   svg
     .append("g")
     .attr("class", "y0 axis")
     .call(y)
     .append("text")
-    .attr("x", -100)
+    .attr("x", -180)
     .attr("y", -40)
     .attr("dominant-baseline", "start")
-    .style("font-size", "14px")
-    .text("Filtration Checks");
+    .style("font-size", config["axis-title-size"])
+    .text(config["y-title"]);
 }
 export { bar };
