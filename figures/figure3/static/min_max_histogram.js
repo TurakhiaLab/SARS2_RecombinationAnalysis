1acc9165f6f6acbb187b7cc9ce43283ab36b8d6f
mrkylesmith <soccerkws@gmail.com>
2025-08-01 23:22:37 -0700

    figure 3 panel d updates

---
+++ figures/figure3/static/min_max_histogram.js
@@ -0,0 +1,112 @@
+import { roundTo } from "../../common/util.js";
+
+async function histogram(svg, config) {
+  // Constants
+  const width =
+      config["width"] - config["margin"].left - config["margin"].right,
+    height = config["height"] - config["margin"].top - config["margin"].bottom;
+
+  const SCATTER_HEIGHT = 150;
+  const SCATTER_WIDTH = width - 200;
+  const SCATTER_RADIUS = 5.0;
+  const SHIFT_POINTS_RIGHT = width / 2;
+  const BAR_PADDING = 2;
+
+  // Get input recomb data and statistics data
+  const statsData = await d3.csv(config["statsFilename"]);
+  const data = await d3.csv(config["filename"]);
+
+  // Format monthly statistics values
+  let statLookup = {};
+  statsData.forEach((d) => {
+    const key = d.Month;
+    if (key in statLookup) {
+      alert("Month already recorded in stats");
+    }
+    statLookup[key] = {
+      Percentile50: parseFloat(d.Percentile50),
+      Percentile75: parseFloat(d.Percentile75),
+      Percentile99: parseFloat(d.Percentile99),
+    };
+  });
+  const binWidth = config["histConfig"].binWidth;
+  const domainStart = config["histConfig"].domainStart;
+  const domainEnd = config["histConfig"].domainEnd;
+
+  let x = d3
+    .scaleLinear()
+    .domain([domainStart, domainEnd])
+    .range([0, SCATTER_WIDTH]);
+
+  const thresholds = d3
+    .range(domainStart + binWidth, domainEnd + binWidth, binWidth)
+    .map((val) => roundTo(val, 2));
+
+  let histogram = d3
+    .bin()
+    .value((d) => parseFloat(d[config["Score"]]))
+    .domain([domainStart, domainEnd])
+    .thresholds(thresholds);
+
+  let bins = histogram(data);
+  const [minCount, maxCount] = d3.extent(bins.map((d) => d.length));
+
+  const OCC_BUFFER = 10;
+  let y = d3
+    .scaleLinear()
+    .domain([0, maxCount + OCC_BUFFER])
+    .range([height, SCATTER_HEIGHT]);
+
+  const xAxis = d3.axisBottom(x).ticks(20).tickSizeOuter(0);
+  const yAxis = d3.axisLeft(y).tickSizeOuter(0);
+
+  // histogram
+  svg
+    .selectAll(".bar")
+    .data(bins)
+    .join("rect")
+    .attr("class", "bar")
+    .attr("x", (d) => {
+      return x(d.x0);
+    })
+    .attr("y", (d) => {
+      return y(d.length);
+    })
+    .attr("width", (d) => {
+      return Math.max(0, x(d.x1) - x(d.x0) - BAR_PADDING);
+    })
+    .attr("height", (d) => {
+      return height - y(d.length);
+    })
+    .style("fill", "#87CEFA")
+    .attr("stroke-width", 0.5)
+    .attr("stroke", "black");
+
+  // x-axis
+  svg
+    .append("g")
+    .attr("class", "bottomAxis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+    .style("font-size", config["axisTickLabelSize"])
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", "rotate(-65)");
+
+  // y-axis
+  svg
+    .append("g")
+    .attr("class", "y0 axis")
+    .call(yAxis)
+    .style("font-size", config["axisTickLabelSize"])
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -300)
+    .attr("y", -80)
+    .attr("dominant-baseline", "central")
+    .style("fill", "black")
+    .style("font-size", "20px");
+}
+export { histogram };
